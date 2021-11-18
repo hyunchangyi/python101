9211f04496ca53bad8497547df540ff1ec6acea7
hyunchangyi <hyunchang.yi@me.com>
2021-11-18 18:48:27 +0900

    Update install_nanum.py

--- script/install_nanum.py
+++ script/install_nanum.py
@@ -2,19 +2,18 @@ import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
 import os
 
-def install_nanum_fonts():
-    nanum_fonts = [f for f in fm.fontManager.ttflist if 'Nanum' in f.name]
+nanum_fonts = [f for f in fm.fontManager.ttflist if 'Nanum' in f.name]
 
-    if len(nanum_fonts) == 0:
-        fm._rebuild()
-        print('Nanum fonts installed. Restart Colab runtime.')
-        # 단계 2: 런타임 재시작
-        os.kill(os.getpid(), 9)
-    else:    
-        print('Nanum fonts are ready for plot.')
-        # 한글 폰트 설정
-        plt.rc('font', family='NanumGothic')
-        # 마이너스 표시 문제
-        plt.rcParams['axes.unicode_minus'] = False
+if len(nanum_fonts) == 0:
+    fm._rebuild()
+    print('Nanum fonts installed. Restart Colab runtime.')
+    # 단계 2: 런타임 재시작
+    os.kill(os.getpid(), 9)
+else:    
+    print('Nanum fonts are ready for plot.')
+    # 한글 폰트 설정
+    plt.rc('font', family='NanumGothic')
+    # 마이너스 표시 문제
+    plt.rcParams['axes.unicode_minus'] = False
         
         
